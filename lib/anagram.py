ad6416ad2e1c09595ef046c6f99d1843fb61a85c
Shadrack Kiprono <shadrackkiprono00@gmail.com>
2025-05-13 13:58:38 -0700

    factor

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        return [
+            candidate for candidate in word_list
+            if candidate.lower() != self.word and
+               sorted(candidate.lower()) == sorted(self.word)
+        ]
+
